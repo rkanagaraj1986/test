bd51d4c348151da9f60d933f6c535cf441f309cf
Radhakrishnan Kanagaraj <rkanagaraj@avaya.com>
2020-10-24 16:44:08 +0530

    update author

---
+++ src/components/Author.vue
@@ -0,0 +1,16 @@
+<template>
+	<h3>Author</h3>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			h: 1,
+			g: 2,
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="row">
+		<div
+			class=""
+			v-for="(quote, index) in quotes"
+			:key="index"
+			@click="delQuote(index)"
+		>
+			<appQuote>{{ quote }}</appQuote>
+		</div>
+	</div>
+</template>
+
+<script>
+import Quote from "./Quote";
+export default {
+	name: "quotegrid",
+	props: ["quotes"],
+	components: { appQuote: Quote },
+	data() {
+		return {
+			data: 0,
+		};
+	},
+	methods: {
+		delQuote(i) {
+			this.$emit("delquote", i);
+		},
+	},
+};
+</script>
+
+<style></style>
